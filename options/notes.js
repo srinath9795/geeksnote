cfd43dee66474eb0e04329cc27cc98329c4962a7
srinath <srinath9795@gmail.com>
2015-09-05 15:38:42 +0430

    options page displaying notes done... simple layout

--- options/notes.js
+++ options/notes.js
@@ -3,19 +3,47 @@
 angular.module('geeksNote',[]).controller('parentController', ['$scope',
 function($scope){
 
-	$scope.notesInit = function(){
-	  	var notesObj = {};
-		
+	// $scope.notesObj = {};
+	// $scope.notesInit = function(){
 		chrome.storage.sync.get("notes", function (obj) {
-		    notesObj=obj.notes;
+		    $scope.notesObj=obj.notes;
 		    
-		    if (typeof notesObj==='undefined') {  // This occurs when the extension is first used as nothing would be stored 
-		        notesObj={};
+		    if (typeof $scope.notesObj==='undefined') {  // This occurs when the extension is first used as nothing would be stored 
+		        $scope.notesObj={};
 		    };
-		    console.log(notesObj);
+			$scope.$apply(function() {
+
+		        console.log("Disk cache hit");
+		        // console.log(value.receiptList);
+		        var p= obj.notes;
+		        var count=0;
+		        var newObj = {};
+
+		        $scope.notesObj = [];
+		        for (var key in p) {
+				  if (p.hasOwnProperty(key)) {
+				    if(!p[key]) continue;
+				    count++;
+				    newObj[key]=p[key];
+				    if (count==3)
+				    {
+				    	$scope.notesObj.push(newObj);
+				    	newObj={};
+				    	count=0;
+				    }
+
+				  }
+				}
+				if (count!=0)
+				{
+				    $scope.notesObj.push(newObj);
+
+				}
+
+		    });
 		});
 
-	}
+	// }
 
 }]);
 
