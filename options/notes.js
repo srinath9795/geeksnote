533c7f9a16d21a56b7104d7282727d7d30a5bdb9
srinath <srinath9795@gmail.com>
2015-09-11 23:38:02 +1000

    basic integration done with codeforces

--- options/notes.js
+++ options/notes.js
@@ -3,8 +3,6 @@
 angular.module('geeksNote',[]).controller('parentController', ['$scope',
 function($scope){
 
-	// $scope.notesObj = {};
-	// $scope.notesInit = function(){
 		chrome.storage.sync.get("notes", function (obj) {
 		    $scope.notesObj=obj.notes;
 		    
@@ -43,7 +41,46 @@ function($scope){
 		    });
 		});
 
-	// }
+
+
+		chrome.storage.sync.get("notesCF", function (obj) {
+		    $scope.notesObj1=obj.notesCF;
+		    
+		    if (typeof $scope.notesObj1==='undefined') {  // This occurs when the extension is first used as nothing would be stored 
+		        $scope.notesObj1={};
+		    };
+			$scope.$apply(function() {
+
+		        console.log("Disk cache hit");
+		        // console.log(value.receiptList);
+		        var p= obj.notesCF;
+		        var count=0;
+		        var newObj = {};
+
+		        $scope.notesObj1 = [];
+		        for (var key in p) {
+				  if (p.hasOwnProperty(key)) {
+				    if(!p[key]) continue;
+				    count++;
+				    newObj[key]=p[key];
+				    if (count==3)
+				    {
+				    	$scope.notesObj1.push(newObj);
+				    	newObj={};
+				    	count=0;
+				    }
+
+				  }
+				}
+				if (count!=0)
+				{
+				    $scope.notesObj1.push(newObj);
+
+				}
+
+		    });
+		});
+
 
 }]);
 
